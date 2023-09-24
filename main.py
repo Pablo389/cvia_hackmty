faa4c11d58a26937e99925a480501cc737ec33aa
Daniel Estrada Ocaña <daniel130403@gmail.com>
2023-09-24 00:36:27 +0000

    adafane

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pickle
 
 original_title = '<p style="font-family:Georgia; color:red; font-size: 100px;">CVIA</p>'
 st.markdown(original_title, unsafe_allow_html=True)
-st.divider('Red')
+st.divider()
 
 uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 #if uploaded_file is not None:
